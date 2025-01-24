cc52b92ea3f322d94d5adba7fd648668ed4da21c
Alexis <180454027+Alexis5631@users.noreply.github.com>
2025-01-23 20:08:40 -0500

    feat: :rocket: inicio del proyecto

---
+++ js/data.js
@@ -0,0 +1,62 @@
+export const dataBase = [
+    {
+        'id': '1',
+        'cod': '0001',
+        'product': 'Resma de papel tamaño carta',
+        'price': 70000
+    },
+    {
+        'id': '2',
+        'cod': '0002',
+        'product': 'Bolígrafo de tinta negra',
+        'price': 1200
+    },
+    {
+        'id': '3',
+        'cod': '0003',
+        'product': 'Cuaderno 7 materias',
+        'price': 60000
+    },
+    {
+        'id': '4',
+        'cod': '0004',
+        'product': 'Marcadores fluorescentes (pack de 5)',
+        'price': 22000
+    },
+    {
+        'id': '5',
+        'cod': '0005',
+        'product': 'Grapadora metálica',
+        'price': 12000
+    },
+    {
+        'id': '6',
+        'cod': '0006',
+        'product': 'Caja de clips',
+        'price': 4000
+    },
+    {
+        'id': '7',
+        'cod': '0007',
+        'product': 'Carpeta de anillas',
+        'price': 5000
+    },
+    {
+        'id': '8',
+        'cod': '0008',
+        'product': 'Tijeras escolares',
+        'price': 8000
+    },
+    {
+        'id': '9',
+        'cod': '0009',
+        'product': 'Cinta adhesiva',
+        'price': 5000
+    },
+    {
+        'id': '10',
+        'cod': '0010',
+        'product': 'Calculadora científica',
+        'price': 25000
+    }
+];
